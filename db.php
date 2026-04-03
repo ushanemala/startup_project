565bc37028c63776e58cb5efe5cd46d4668a2b36
ushanemala <ushanemala05@gmail.com>
2026-04-03 20:41:12 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,10 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "startup_project");
+$conn = new mysqli(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE')
+);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
